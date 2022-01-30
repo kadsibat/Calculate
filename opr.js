07895fa330a19edb18239ad12ec03638b595c5b1
Batuhan <yesiltas_sibel@hotmail.com>
2022-01-30 18:45:58 +0300

    düzeltme

--- opr.js
+++ opr.js
@@ -1,19 +1,20 @@
-let num1= +prompt("Please enter the Number1:");
-let operator= prompt("${`+`,`-`,`*`,`/`}:");
-let num2= +prompt("Please enter the Number2:");
+let num1= +prompt(`Please enter the Number1:`);
+let operator= prompt(`Please enter the operator:`);
+let num2= +prompt(`Please enter the Number2:`);
+
 let calculate;
-switch (operator) {
+switch (operator){
   case "+":
-    calculate=num1+num2;
+    calculate=num1+num2
     break;
   case "-":
-    calculate =num1-num2;
+    calculate =num1-num2
     break;
   case "*":
-    calculate = "num1*num2";
+    calculate = num1*num2
     break;
   case "/":
-    calculate = "num1/num2";
+    calculate = num1/num2
     break;
   default:
   	calculate="Invalid operator";
