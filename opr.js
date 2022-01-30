92b349896cc3b361ed874f9a82b9112bf1c8a9c5
Batuhan <yesiltas_sibel@hotmail.com>
2022-01-30 15:12:39 +0300

    javascrpt

---
+++ opr.js
@@ -0,0 +1,22 @@
+let num1= +prompt("Please enter the Number1:");
+let operator= prompt("${`+`,`-`,`*`,`/`}:");
+let num2= +prompt("Please enter the Number2:");
+let calculate;
+switch (operator) {
+  case "+":
+    calculate=num1+num2;
+    break;
+  case "-":
+    calculate =num1-num2;
+    break;
+  case "*":
+    calculate = "num1*num2";
+    break;
+  case "/":
+    calculate = "num1/num2";
+    break;
+  default:
+  	calculate="Invalid operator";
+    break;
+}
+console.log(calculate);
